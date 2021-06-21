8743fea8ea228bdeba6b79ad2060760457f963e7
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-06-21 20:25:12 +0200

    Add control for multiple leaderboards

--- pages/api/cron/leaderboard.js
+++ pages/api/cron/leaderboard.js
@@ -1,5 +1,9 @@
 const fetch = require("node-fetch");
 const db = require("../../../db");
+const { ParameterizedQuery: PQ } = require('pg-promise');
+
+// Creating a complete Parameterized Query with parameters:
+
 
 export default async (req, res) => {
     let leaderboardResponse = await fetch("https://pokemongolive.com/_api/gbl.get_leaderboard", {
@@ -13,8 +17,20 @@ export default async (req, res) => {
         method: "POST",
     })
     const leaderboard = await leaderboardResponse.json();
-    
+    console.log("Got leaderboard for date: ", new Date(leaderboard.last_updated));
+    const findUpdated = new PQ({ text: 'SELECT * FROM LB_UPDATED WHERE updated = $1', values: [new Date(leaderboard.last_updated)] });
 
+    try {
+        const alreadyIn = db.db.one(findUpdated);
+        if (alreadyIn) {
+            res.json({
+                ok: false,
+                error: `Leaderboard for date ${new Date(leaderboard.last_updated)} already present.`
+            });
+            return;
+        }
+    } catch (e) { console.error(e) }
+    console.log("If we are here we have a new leaderboard to insert!");
     // our set of columns, to be created only once (statically), and then reused,
     // to let it cache up its formatting templates for high performance:
     const cs = new db.pgp.helpers.ColumnSet(['trainer', 'team', 'rating', 'rank', 'position', 'battles', 'updated_at'], { table: 'LB_HISTORY' });
