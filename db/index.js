8e5cab312fb1c7303d135dc5916524a015d1298c
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-06-21 19:45:14 +0200

    Fixed db connection

--- db/index.js
+++ db/index.js
@@ -1,13 +1,7 @@
 const pgp = require('pg-promise')();
 
 console.log("Initializing db");
-const db = pgp({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-});
+const db = pgp(process.env.DATABASE_URL);
 
 module.exports = {
     pgp,
