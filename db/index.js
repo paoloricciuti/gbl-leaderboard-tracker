7de7cc8b938ac5730c35e49fb602d475fd96455a
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-06-21 19:50:46 +0200

    Added ssl for db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')();
 
 console.log("Initializing db");
-const db = pgp(process.env.DATABASE_URL);
+const db = pgp(`${process.env.DATABASE_URL}?ssl=true`);
 
 module.exports = {
     pgp,
