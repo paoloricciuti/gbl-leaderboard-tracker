fe7f7ef3cac56fbc458f8f40b2915014f17b44c2
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-06-21 20:02:25 +0200

    removed ssl

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')();
 
 console.log("Initializing db");
-const db = pgp(`${process.env.DATABASE_URL}?ssl=true`);
+const db = pgp(process.env.DATABASE_URL);
 
 module.exports = {
     pgp,
